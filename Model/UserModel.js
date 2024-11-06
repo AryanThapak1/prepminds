526e1f6e4723587f8a4e7f450d3bf0ff24d55447
ARYAN THAPAK <aryanthapak.8@gmail.com>
2024-11-07 00:23:21 +0530

    v1.0

--- Model/UserModel.js
+++ Model/UserModel.js
@@ -44,6 +44,10 @@ const userSchema = new mongoose.Schema({
   changedPasswordAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  Resume:{
+    type:mongoose.Types.ObjectId,
+    ref:Resume
+  }
 });
 
 userSchema.pre("save", async function (next) {
